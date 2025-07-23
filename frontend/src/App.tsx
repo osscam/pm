f1a6fd98d305801631a1a4b2857d17207488aba7
itsvisal <hello@chivanvisal.com>
2025-07-23 16:04:49 +0700

    feat: update index.html title, integrate ThemeProvider, and enhance routing structure with layouts

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,8 +1,13 @@
 import { RouterProvider } from "react-router-dom";
 import { router } from "./routers";
+import { ThemeProvider } from "./providers/theme-provider";
 
 const App = () => {
-  return <RouterProvider router={router} />;
+  return (
+    <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
+      <RouterProvider router={router} />
+    </ThemeProvider>
+  );
 };
 
 export default App;
--- frontend/src/routers.tsx
+++ frontend/src/routers.tsx
@@ -1,8 +1,33 @@
 import { createBrowserRouter } from "react-router-dom";
+import RootLayout from "./components/layouts/RootLayout";
+import DashboardLayout from "./components/layouts/DashboardLayout";
+import AuthLayout from "./components/layouts/AuthLayout";
 
 export const router = createBrowserRouter([
   {
     path: "/",
-    element: <div>Hello World</div>,
+    element: <RootLayout />,
+    children: [
+      {
+        path: "dashboard",
+        element: <DashboardLayout />,
+        children: [
+          {
+            index: true,
+            element: <div>Dashboard</div>,
+          },
+        ],
+      },
+      {
+        path: "auth",
+        element: <AuthLayout />,
+        children: [
+          {
+            index: true,
+            element: <div>Login</div>,
+          },
+        ],
+      },
+    ],
   },
 ]);
